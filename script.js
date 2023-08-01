68748614ce4c28c3ed57daa76b7ce397a6c65ff6
Himansu Sekhar Lenka <himansu.lenka173@gmail.com>
2023-08-01 21:03:53 +0530

    final commit

---
+++ script.js
@@ -0,0 +1,34 @@
+function handleMouseOver(){
+    document.querySelector(`.view`).innerHTML=`<img src="images/img1.png" alt="img1" width="100%">`;
+}
+
+document.querySelector(`.preview :nth-child(1)`).addEventListener('mouseover', function handleMouseOver() {
+    document.querySelector(`.view`).innerHTML=`<img src="images/img1.png" alt="img1" width="100%">`;
+})
+document.querySelector(`.preview :nth-child(2)`).addEventListener('mouseover', function handleMouseOver() {
+    document.querySelector(`.view`).innerHTML=`<img src="images/img2.png" alt="img1" width="100%">`;
+})
+document.querySelector(`.preview :nth-child(3)`).addEventListener('mouseover', function handleMouseOver() {
+    document.querySelector(`.view`).innerHTML=`<img src="images/img3.png" alt="img1" width="100%">`;
+})
+document.querySelector(`.preview :nth-child(4)`).addEventListener('mouseover', function handleMouseOver() {
+    document.querySelector(`.view`).innerHTML=`<img src="images/img4.png" alt="img1" width="100%">`;
+})
+document.querySelector(`.preview :nth-child(5)`).addEventListener('mouseover', function handleMouseOver() {
+    document.querySelector(`.view`).innerHTML=`<img src="images/img5.png" alt="img1" width="100%">`;
+})
+document.querySelector(`.preview :nth-child(6)`).addEventListener('mouseover', function handleMouseOver() {
+    document.querySelector(`.view`).innerHTML=`<img src="images/img6.png" alt="img1" width="100%">`;
+})
+document.querySelector(`.preview :nth-child(7)`).addEventListener('mouseover', function handleMouseOver() {
+    document.querySelector(`.view`).innerHTML=`<img src="images/img7.png" alt="img1" width="100%">`;
+})
+document.querySelector(`.preview :nth-child(8)`).addEventListener('mouseover', function handleMouseOver() {
+    document.querySelector(`.view`).innerHTML=`<img src="images/img8.png" alt="img1" width="100%">`;
+})
+document.querySelector(`.preview :nth-child(9)`).addEventListener('mouseover', function handleMouseOver() {
+    document.querySelector(`.view`).innerHTML=`<img src="images/img9.png" alt="img1" width="100%">`;
+})
+document.querySelector(`.preview :nth-child(10)`).addEventListener('mouseover', function handleMouseOver() {
+    document.querySelector(`.view`).innerHTML=`<img src="images/img10.png" alt="img1" width="100%">`;
+})
